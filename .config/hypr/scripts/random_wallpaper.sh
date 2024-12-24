9be3aecf062c9c2ed5abdc3f2998918ecbc05aa7
sawongam <sangamadhikari.61@gmail.com>
2024-12-24 23:00:09 +0545

    dual monitor wallpaper setup

--- .config/hypr/scripts/random_wallpaper.sh
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -3,12 +3,15 @@
 # Directory containing wallpapers
 WALLPAPER_DIR="$HOME/.wallpapers"
 
-# Randomly select a wallpaper
-RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+# Randomly select a wallpaper for each display
+RANDOM_WALLPAPER_PRIMARY=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+RANDOM_WALLPAPER_SECONDARY=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
 
 # Update Hyprpaper config
-echo "preload = $RANDOM_WALLPAPER" > "$HOME/.config/hypr/hyprpaper.conf"
-echo "wallpaper = eDP-1, contain:$RANDOM_WALLPAPER" >> "$HOME/.config/hypr/hyprpaper.conf"
+echo "preload = $RANDOM_WALLPAPER_PRIMARY" > "$HOME/.config/hypr/hyprpaper.conf"
+echo "preload = $RANDOM_WALLPAPER_SECONDARY" >> "$HOME/.config/hypr/hyprpaper.conf"
+echo "wallpaper = eDP-1, contain:$RANDOM_WALLPAPER_PRIMARY" >> "$HOME/.config/hypr/hyprpaper.conf"
+echo "wallpaper = HDMI-A-1, contain:$RANDOM_WALLPAPER_SECONDARY" >> "$HOME/.config/hypr/hyprpaper.conf"
 echo "ipc = off" >> "$HOME/.config/hypr/hyprpaper.conf"
 
 # Kill any running instance of Hyprpaper
